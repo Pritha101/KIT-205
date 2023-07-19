2699c441db090f8884d751962e38346899d00343
Pritha Shrestha <pritha.shrestha@utas.edu.au>
2023-07-19 14:00:01 +1000

    Tersting 2 part 2

--- Project1/wk2main.c
+++ Project1/wk2main.c
@@ -5,26 +5,34 @@
 
 
 int main() {
-   /* List list = new_list();
+   //List list = new_list();
 
-    insert_at_front(&list, 1);
-    insert_at_front(&list, 2);
-    print_list(&list);*/ // this is the first testing part.
+    //insert_at_front(&list, 1);
+    //insert_at_front(&list, 2);
+    //print_list(&list); // this is the first testing part.
 
     List list = new_list();
     int i = -1;
-    printf("Number:");
-    scanf_s("%d", &i);
    
 
     while (i != 0) {
         scanf_s("%d", &i);
         insert_at_front(&list, i);
         
-        
     }
 
     print_list(&list);
 
+    destroy_list(&list);
+
     return 0;
 }
+
+/*int main() {
+    List my_list = new_list();
+    int quit
+
+
+    return 0;
+}*/
+
