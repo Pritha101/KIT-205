6fcfb8e0a6a843cd25b1d4137dd9605a083a857d
Pritha Shrestha <pritha.shrestha@utas.edu.au>
2023-07-19 13:47:26 +1000

    Testing2

--- Project1/wk2main.c
+++ Project1/wk2main.c
@@ -1,13 +1,30 @@
-//first testing part
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
 
 int main() {
-    List list = new_list();
+   /* List list = new_list();
 
     insert_at_front(&list, 1);
     insert_at_front(&list, 2);
+    print_list(&list);*/ // this is the first testing part.
+
+    List list = new_list();
+    int i = -1;
+    printf("Number:");
+    scanf_s("%d", &i);
+   
+
+    while (i != 0) {
+        scanf_s("%d", &i);
+        insert_at_front(&list, i);
+        
+        
+    }
+
     print_list(&list);
+
+    return 0;
 }
